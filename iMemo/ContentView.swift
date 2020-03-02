677534c4d9b71e273cae84554e3851ba831e6475
lee_jjoon <whitour@gmail.com>
2020-03-02 21:42:10 +0900

    Initial Commit

---
+++ iMemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iMemo
+//
+//  Created by Seung Joon Lee on 2020/03/02.
+//  Copyright © 2020 Seung Joon Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
